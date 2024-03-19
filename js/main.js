a8744983069a5456294f3ab5b6fc9c2fb7df2e97
unknown <emilygb88@gmail.com>
2024-03-19 13:55:07 -0400

    Changed the format of the tooltips a bit for increased readability

--- js/main.js
+++ js/main.js
@@ -5,14 +5,17 @@ d3.csv('data/ufo_sightings.csv')
 .then(data => {
     console.log(data[0]);
     console.log(data.length);
-    data.forEach(d => {
-      console.log(d);
-      d.latitude = +d.latitude; //make sure these are not strings
-      d.longitude = +d.longitude; //make sure these are not strings
-    });
+    const filteredData = data.filter(d => d.latitude !== "NA" && d.longitude !== "NA");
+        //for some reason theres a data point (row 43784 in ufo_sightings) where the latitude doesn't exist
+        //it prevents the data from loading on the map so i filtered it out
+        filteredData.forEach(d => {
+            //console.log(d);
+            d.latitude = +d.latitude; //make sure these are not strings
+            d.longitude = +d.longitude; //make sure these are not strings
+        });
 
     // Initialize chart and then show it
-    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, filteredData);
 
 
   })
